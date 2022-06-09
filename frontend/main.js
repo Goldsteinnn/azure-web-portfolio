54baf95a1b198f212b3eb047602518de87c01fc5
goldsteinnn <huynhgoldstein@gmail.com>
2022-06-09 15:18:50 -0500

    added icons

--- frontend/main.js
+++ frontend/main.js
@@ -2,12 +2,12 @@ window.addEventListener('DOMContentLoaded', (event) =>{
     getVisitCount();
 })
 
-
-const functionApi = '';
+const functionApiURL = 'https://getresumecounterr.azurewebsites.net/api/GetResumeCounter?code=ap8-ViUWwg5fIHaH6ZUnFl59tjaLrHlfDN0lhUgIU0OUAzFus4sxHw=='
+const functionApi = 'http://localhost:7071/api/GetResumeCounter';
 
 const getVisitCount = () => {
     let count = 30;
-    fetch(functionApi).then(Response => {
+    fetch(functionApiURL).then(Response => {
         return Response.json()
     }).then(Response => {
         console.log("Website called function api");
