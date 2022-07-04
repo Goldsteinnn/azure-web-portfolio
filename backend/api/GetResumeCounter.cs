1247dba408967c35ed462cfe435d50e3e83b2ec0
Goldstein Huynh <huynhgoldstein@gmail.com>
2022-07-04 12:40:20 -0500

    hotfix for not finding resource

--- backend/api/GetResumeCounter.cs
+++ backend/api/GetResumeCounter.cs
@@ -14,7 +14,7 @@ namespace Company.Function
 {
     public static class GetResumeCounter
     {
-        // 
+        
         [FunctionName("GetResumeCounter")]
         public static HttpResponseMessage Run(
             [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
